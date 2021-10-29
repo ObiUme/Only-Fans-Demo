018e8661d96d7f0517d06ff159f192737aa99001
Obi <mr.obiume@gmail.com>
2021-10-29 16:56:16 -0700

    flex divs

---
+++ client/src/components/OnlyMain.js
@@ -0,0 +1,16 @@
+function OnlyMain(){
+    return(
+        <>
+        <div style={{ float: 'left', width: '31%'}}>
+            Hello
+        </div>
+        <div style={{ float: 'left', width: '31%', margin: '0 0 0 3.5%'}}>
+            there
+        </div>
+        <div style={{float: 'right', width: '31%'}}>
+            Friend
+        </div>
+        </>
+    )
+}
+export default OnlyMain;
